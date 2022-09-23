0ecaa5990328e135119881d03af252d9322a0c4f
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-09-23 13:14:00 +1000

    extract view from game controller

--- controller/gameController.py
+++ controller/gameController.py
@@ -6,6 +6,7 @@ from model.game.tetronomo import Tetronomo
 import model.game.shapesData as sd
 from model.game.grid import PlayField
 import view.menu.topScoreScreen as topScoreScreen
+from view.game.gameView import GameView
 
 import pygame
 import random
@@ -13,15 +14,8 @@ import random
 class GameController():
     def __init__(self, controller):
             pygame.font.init()
+            self.gameView = GameView(self, pygame, controller.surface)
             self.controller = controller
-            self.surface = controller.surface
-            self.s_width = controller.config['screenSize']['width']
-            self.s_height = controller.config['screenSize']['height']
-            self.play_width = 300  # meaning 300 // 10 = 30 width per block
-            self.play_height = 600  # meaning 600 // 20 = 20 height per block
-            self.block_size = (self.play_width if (self.play_width<self.play_height) else self.play_height)//10
-            self.top_left_x = (self.s_width - self.play_width)//2
-            self.top_left_y = self.s_height - self.play_height
             self.score = 0
             self.playField = PlayField()
             self.tetroLanded = False
@@ -32,8 +26,6 @@ class GameController():
             self.fallTime = 0
             self.fallSpeed = 0.27
             self.pause = False
-            self.win = pygame.display.set_mode((self.s_width, self.s_height))
-            pygame.display.set_caption('Tetris')
             self.startGame()  # start game
 
     def showPopUpBox(self):
@@ -58,55 +50,7 @@ class GameController():
                 return True
         return False
     
-    def drawPlayField(self):
-        sx = self.top_left_x
-        sy = self.top_left_y
-
-        for i in range(len(self.playField.grid)):
-            pygame.draw.line(self.surface, (128,128,128), (sx, sy+i*self.block_size), (sx+self.play_width, sy+i*self.block_size))
-            for j in range(len(self.playField.grid[i])+1):
-                pygame.draw.line(self.surface, (128,128,128), (sx+j*self.block_size, sy), (sx+j*self.block_size, sy+self.play_height))
-
-
-    def drawNextTetronomo(self,tetronomo):
-        font = pygame.font.SysFont('arial', 30)
-        label = font.render("Next Shape", 1, (255,255,255))
-
-        sx = self.top_left_x + self.play_width +50
-        sy = self.top_left_y + self.play_height/2-100
-        format = tetronomo.shape[tetronomo.rotation % len(tetronomo.shape)]
-
-        for i, line in enumerate(format):
-            row = list(line)
-            for j, column in enumerate(row):
-                if column == '0':
-                    pygame.draw.rect(self.surface, tetronomo.colour, (sx+j*self.block_size, sy+i*self.block_size, self.block_size, self.block_size),0)
-        self.surface.blit(label, (sx+10, sy-30))
-
-    def drawWindow(self, playField):
-        self.surface.fill((0,0,0))
-        pygame.font.init()
-        font = pygame.font.SysFont('arial', 60)
-        menuFont = pygame.font.SysFont('arial', 24)
-
-        fontLabel = font.render('Tetris', 1, (255,255,255))
-        label2 = menuFont.render('Group: 10', 1, (255,255,255))
-        scoreLabel = menuFont.render(f'Score: {self.score}', 1, (255,255,255))
-        levelLabel = menuFont.render('Level: 1', 1, (255,255,255))
-        modeLabel = menuFont.render('Mode: Player', 1, (255,255,255))
-
-        self.surface.blit(fontLabel, (0+self.s_width//2 - fontLabel.get_width()//2, 30))
-        self.surface.blit(label2, (0, 15))
-        self.surface.blit(scoreLabel, (0+self.s_width - scoreLabel.get_width(), 15))
-        self.surface.blit(levelLabel, (0+self.s_width *4/5 - levelLabel.get_width(), 15))
-        self.surface.blit(modeLabel, (self.s_width*1/5, 15))
-
-        for i in range(len(playField)):
-            for j in range(len(playField[i])):
-                pygame.draw.rect(self.surface, playField[i][j], (self.top_left_x+j*self.block_size, self.top_left_y+i*self.block_size, self.block_size, self.block_size), 0) #Draw blocks
-
-        self.drawPlayField()
-        
+    
 
 
     def startGame(self):
@@ -173,14 +117,12 @@ class GameController():
                 self.tetroLanded = False
                 clearedRows = self.playField.clearRows()
                 self.score += 50*clearedRows**2+50*clearedRows
-                
-                
-            self.drawWindow(self.playField.grid)
-            self.drawNextTetronomo(self.nextTetronomo)
+            self.gameView.drawWindow(self.playField.grid)
+            self.gameView.drawNextTetronomo(self.nextTetronomo)
             pygame.display.update()
 
             if self.loseCondition():
-                run = False
+                self.run = False
         topScoreScreen.HighScoreScreen(self.controller)
 
   
---
+++ view/game/gameView.py
@@ -0,0 +1,64 @@
+class GameView():
+    def __init__(self,controller,pygame, surface):
+        self.pygame = pygame
+        self.controller = controller
+        self.surface = surface
+        self.s_width, self.s_height = self.surface.get_size()
+        self.play_width = 300
+        self.play_height = 600
+        self.block_size = (self.play_width if (self.play_width<self.play_height) else self.play_height)//10
+        self.top_left_x = (self.s_width - self.play_width)//2
+        self.top_left_y = self.s_height - self.play_height
+        self.win = pygame.display.set_mode((self.s_width, self.s_height))
+        pygame.display.set_caption('Tetris')
+
+
+    def drawPlayField(self):
+        sx = self.top_left_x
+        sy = self.top_left_y
+
+        for i in range(len(self.controller.playField.grid)):
+            self.pygame.draw.line(self.surface, (128,128,128), (sx, sy+i*self.block_size), (sx+self.play_width, sy+i*self.block_size))
+            for j in range(len(self.controller.playField.grid[i])+1):
+                self.pygame.draw.line(self.surface, (128,128,128), (sx+j*self.block_size, sy), (sx+j*self.block_size, sy+self.play_height))
+
+
+    def drawNextTetronomo(self,tetronomo):
+        font = self.pygame.font.SysFont('arial', 30)
+        label = font.render("Next Shape", 1, (255,255,255))
+
+        sx = self.top_left_x + self.play_width +50
+        sy = self.top_left_y + self.play_height/2-100
+        format = tetronomo.shape[tetronomo.rotation % len(tetronomo.shape)]
+
+        for i, line in enumerate(format):
+            row = list(line)
+            for j, column in enumerate(row):
+                if column == '0':
+                    self.pygame.draw.rect(self.surface, tetronomo.colour, (sx+j*self.block_size, sy+i*self.block_size, self.block_size, self.block_size),0)
+        self.surface.blit(label, (sx+10, sy-30))
+
+    def drawWindow(self, playField):
+        self.surface.fill((0,0,0))
+        self.pygame.font.init()
+        font = self.pygame.font.SysFont('arial', 60)
+        menuFont = self.pygame.font.SysFont('arial', 24)
+
+        fontLabel = font.render('Tetris', 1, (255,255,255))
+        label2 = menuFont.render('Group: 10', 1, (255,255,255))
+        scoreLabel = menuFont.render(f'Score: {self.controller.score}', 1, (255,255,255))
+        levelLabel = menuFont.render('Level: 1', 1, (255,255,255))
+        modeLabel = menuFont.render('Mode: Player', 1, (255,255,255))
+
+        self.surface.blit(fontLabel, (0+self.s_width//2 - fontLabel.get_width()//2, 30))
+        self.surface.blit(label2, (0, 15))
+        self.surface.blit(scoreLabel, (0+self.s_width - scoreLabel.get_width(), 15))
+        self.surface.blit(levelLabel, (0+self.s_width *4/5 - levelLabel.get_width(), 15))
+        self.surface.blit(modeLabel, (self.s_width*1/5, 15))
+
+        for i in range(len(playField)):
+            for j in range(len(playField[i])):
+                self.pygame.draw.rect(self.surface, playField[i][j], (self.top_left_x+j*self.block_size, self.top_left_y+i*self.block_size, self.block_size, self.block_size), 0) #Draw blocks
+
+        self.drawPlayField()
+        
